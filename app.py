4957b8a6e48ae9ec00609ab5969aa72247424059
Christopher Holmes <christopher.holmes@newcastle.co.uk>
2020-10-23 16:29:51 +0100

    Added comments

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def my_func():
+    # define variables to pass through to the template
     currentUser = "Chris"
     users = ["Gav", "Chris", "Eddy"]
+
+    # return the template when user accesses the root, while passing through the created variables
     return render_template('index.html', user=currentUser, userList=users)
 
 @app.route('/test_route')
 def my_func_2():
+    # return raw html as a string
     return '<h2>Hi from /test_route!</h2>'
 
 if __name__ == "__main__":
